fba2c2f24d27d4b4981837c8d8052d0725bdb700
Akhila Nori <80121020+akhila-nori@users.noreply.github.com>
2024-08-12 20:42:19 +0530

    added Title and Header component with different css in App.js

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const TitleComponent = () => {
+  return <h2 id="anila">Hello title component</h2>;
+};
+
 const HeadingComponent = () => {
   return (
     <div id="akhila">
+      <TitleComponent />
       <h1>HELLO FUNCTIONAL COMPONENT - AKHILA</h1>
     </div>
   );
