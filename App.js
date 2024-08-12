640eeebee0f8e2a1afa62b367aa29c7603c59fb3
Akhila Nori <80121020+akhila-nori@users.noreply.github.com>
2024-08-12 18:25:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+// {
+/* <div id="parent">
+     <div id="child">
+        <h1>Hey, I'm h1 tag sibling of h2 tag and child of div id child </h1>
+        <h2>Hey, I'm h2 tag sibling of h1 tag and child of div id child </h2>
+     </div>
+   </div> */
+// }
+
+// code this up and create a REACT DOM for the above - sibling - convert into array of different children
+//and wrap the siblings inside the square brackets
+
+//here the 3rd argument is the children that you have to pass in
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "akhila" }, [
+    React.createElement("h1", {}, "Hey, I'm h1 tag"),
+    React.createElement("h2", {}, "Hey, I'm h2 tag"),
+  ])
+);
+
+//JSX
+console.log(parent);
+
+
+
+
