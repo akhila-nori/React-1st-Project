ee6a69187a39b724db05d0c84f24e43d8e75f552
Akhila Nori <80121020+akhila-nori@users.noreply.github.com>
2024-08-12 20:56:34 +0530

    injecting dyanmic values {} and javascript expressions inside jsx

--- App.js
+++ App.js
@@ -5,9 +5,12 @@ const TitleComponent = () => {
   return <h2 id="anila">Hello title component</h2>;
 };
 
+const number = 10000;
+
 const HeadingComponent = () => {
   return (
     <div id="akhila">
+      <h2>{number}</h2>
       <TitleComponent />
       <h1>HELLO FUNCTIONAL COMPONENT - AKHILA</h1>
     </div>
