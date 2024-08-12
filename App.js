2b9507be9fcc13ccba5a1c5c7b4c9d89594d2881
Akhila Nori <80121020+akhila-nori@users.noreply.github.com>
2024-08-12 21:22:33 +0530

    component composition in App.js

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//React element / simply javascript variable
+const elem = (
+  <span>
+    React Element
+    <br />
+  </span>
+);
+
+const title = (
+  <h1 tabIndex="5">
+    {elem}
+    Namaste React inside title javascript object
+  </h1>
+);
+
 const TitleComponent = () => {
   return <h2 id="anila">Hello title component</h2>;
 };
@@ -10,6 +25,7 @@ const number = 10000;
 const HeadingComponent = () => {
   return (
     <div id="akhila">
+      {title}
       <h2>{number}</h2>
       <TitleComponent />
       <h1>HELLO FUNCTIONAL COMPONENT - AKHILA</h1>
